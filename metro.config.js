75a7e6e1aa2197e242bbecf27c685513a42e6d12
i-ganza007 <i.ganza@alustudent.com>
2026-03-02 01:39:47 +0200

    clear cache

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Required so require('./assets/models/*.tflite') is bundled in dev and production
-config.resolver.assetExts.push('tflite');
-
-module.exports = config;
